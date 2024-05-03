de15d515f9872fc3319c56fbda3ea0e058a0880a
Udo Offermann <udo.offermann@zfabrik.de>
2024-05-03 23:11:53 +0200

    store daily yield into postgres

--- logger/de.atrium.solarlogger.main/java/src.impl/de/atrium/solarlogger/impl/DailyYieldDAO.java
+++ logger/de.atrium.solarlogger.main/java/src.impl/de/atrium/solarlogger/impl/DailyYieldDAO.java
@@ -37,20 +37,34 @@ public class DailyYieldDAO {
         }
     }
 
-    public void save(LocalDate date, int address, Double yield) {
+    public void save(LocalDate date, int address, double yield) {
+        if (yield > 0) {
+            updateYield(date, address, yield);
+        } else {
+            insertNewDay(date, address);
+        }
+    }
+
+    private void insertNewDay(LocalDate date, int address) {
         try (Connection con = DriverManager.getConnection(JDBC_URL, DB_PROPS)) {
             try (PreparedStatement insert = con.prepareStatement(
-                    // UPSERT yield - update only if new yield > previous yield
-                    "INSERT INTO daily_yield (day, inverter, yield) VALUES (?, ?, ?) " +
-                    "ON CONFLICT (day, inverter) DO UPDATE SET yield=EXCLUDED.yield, updated_at=now() "+
-                    "WHERE daily_yield.yield < EXCLUDED.yield")) {
+                    // Insert first entry of new day:
+                    //  if this happens before midnight, (day, inverter) already exists and we do nothing
+                    //  if this happens after midnight, a new entry for the new day is stored with yield = 0
+                    "INSERT INTO daily_yield (day, inverter, 0) " +
+                    "                 VALUES (?,   ?,        ?) " +
+                    "                 ON CONFLICT (day, inverter) DO NOTHING")) {
 
                 insert.setObject(1, date);
                 insert.setInt(2, address);
-                insert.setDouble(3, yield);
 
-                insert.execute();
-                LOGGER.info("Upsert (" + date + ", " + address + ", " + yield + ") into daily_yield table");
+                if (insert.executeUpdate() > 0) {
+                    // first insert of new day
+                    LOGGER.info("Insert (" + date + ", " + address + ", " + 0 + ") into daily_yield table");
+                } else {
+                    // second insert of new day or inverter was reset before midnight
+                    LOGGER.info("Skipped Insert (" + date + ", " + address + ", " + 0 + ") into daily_yield table");
+                }
             } catch (SQLException e) {
                 LOGGER.warning("IGNORE: Failed to insert yield because " + e);
             }
@@ -59,4 +73,31 @@ public class DailyYieldDAO {
         }
     }
 
+    private void updateYield(LocalDate date, int address, double yield) {
+        try (Connection con = DriverManager.getConnection(JDBC_URL, DB_PROPS)) {
+            try (PreparedStatement insert = con.prepareStatement(
+                    // yield must increase monotonically!
+                    // If this happens before midnight, the yield is updated in a monotonically increasing manner
+                    // If this happens after midnight, the update will not find a matching row and we do nothing
+                    "UPDATE daily_yield SET yield = ?, updated_at = now() WHERE day=? AND inverter=? and ? > yield")) {
+
+                insert.setDouble(1, yield);
+                insert.setObject(2, date);
+                insert.setInt(3, address);
+                insert.setDouble(4, yield);
+
+                if (insert.executeUpdate() > 0) {
+                    // new yield > old yield
+                    LOGGER.info("Update (" + date + ", " + address + ", " + yield + ") in daily_yield table");
+                } else {
+                    // new yield <= old yield or new day
+                    LOGGER.info("Skipped Update (" + date + ", " + address + ", " + yield + ") in daily_yield table");
+                }
+            } catch (SQLException e) {
+                LOGGER.warning("IGNORE: Failed to update yield because " + e);
+            }
+        } catch (SQLException e) {
+            LOGGER.warning("IGNORE: Failed to access DB " + JDBC_URL + " because " + e);
+        }
+    }
 }
